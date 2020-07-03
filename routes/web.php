29365e67a811ac5a0202cf0648f3630e86f385f4
kevinanggaaa <kevin.anggahung@gmail.com>
2020-07-03 10:48:29 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/pertanyaan', 'pertanyaanController@index');
+
+Route::get('/pertanyaan/create', 'PertanyaanController@create');
+
+Route::post('/pertanyaan', 'PertanyaanController@store');
+
+Route::get('/jawaban/create', 'JawabanController@create');
+
+Route::get('/jawaban/{pertanyaan_id}', 'JawabanController@index');
+
+Route::post('/jawaban', 'JawabanController@store');
+
